13119d93c9210eb295b5fb10932d3c1646fe23ec
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 04:21:01 +0000

    Adding tarfile member sanitization to extractall()

--- modules/cmake.py
+++ modules/cmake.py
@@ -47,7 +47,26 @@ def prepare_if_archive(src):
     elif os.path.isfile(src) and tarfile.is_tarfile(src):
         result = mkdtemp()
         with tarfile.open(src) as tar:
-            tar.extractall(result)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, result)
     elif os.path.isfile(src) and zipfile.is_zipfile(src):
         result = mkdtemp()
         with zipfile.ZipFile(src) as zip:
